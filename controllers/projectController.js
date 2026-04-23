18f83a8d29d424cdb3fcda2bf409de3594bd5ddc
M4her <maher44hmed@gmail.com>
2026-04-23 23:56:19 +0600

    projectListAndSearch

--- controllers/projectController.js
+++ controllers/projectController.js
@@ -4,7 +4,7 @@ const projectSchema = require("../models/projectSchema");
 const createProject = async (req, res) => {
   const { title, description } = req.body;
   try {
-    const slug = generateSlug(title)
+    const slug = generateSlug(title);
     const project = await projectSchema({
       title,
       description,
@@ -13,10 +13,33 @@ const createProject = async (req, res) => {
     });
     project.save();
 
-    res.status(200).send({message: "Project Created Successfully"})
+    res.status(200).send({ message: "Project Created Successfully" });
   } catch (error) {
-    console.log(error)
+    console.log(error);
+    res.status(500).send({ message: "Internal Server Error" });
   }
 };
 
-module.exports = { createProject };
+const projectList = async (req, res) => {
+  try {
+    const { search } = req.query;
+    // console.log(search);
+
+    const projects = await projectSchema.find({
+      author: req.user._id,
+      title: {
+        $regex: search,
+        $options: "i",
+      },
+    });
+    if (!projects)
+      return res.status(400).send({ message: "Project not found" });
+    res.status(200).send({ projects });
+  } catch (error) {
+    res.status(500).send({ message: "Internal Server Error" });
+  }
+};
+
+
+
+module.exports = { createProject, projectList };
